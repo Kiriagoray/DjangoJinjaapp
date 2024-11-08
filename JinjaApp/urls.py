7778ef0d3a42e2d9f8f8fda2b79925631fb87bd5
kiriagoray <kiriagoraymond@gmail.com>
2024-11-08 15:03:48 +0300

    added files

---
+++ JinjaApp/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path
+from JinjaApp import views
+
+urlpatterns=[
+
+    path('',views.home,name='my_home'),
+    path('contact',views.contact,name='my_contact'),
+    path('about',views.about,name='my_about'),
+
+
+]
+
+
+
